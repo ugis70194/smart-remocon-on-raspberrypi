b54d73c474029fe0d280fb0396f171e50ee9bbc7
ugis70194 <ugis70194@yahoo.co.jp>
2023-04-24 10:17:27 +0000

    レスポンスのテストを追加

--- APIserver/test.py
+++ APIserver/test.py
@@ -3,10 +3,23 @@ import requests
 
 class TestHTTPResponse(unittest.TestCase):
   
-  def test_light_on(self):
+  def test_reach_light_on(self):
     req = requests.get("http://localhost:3000/light_on")
     self.assertEqual(req.status_code, 200)
     
-  def test_light_off(self):
+  def test_response_light_on(self):
+    req = requests.get("http://localhost:3000/light_on")
+    response_code = req.json()["response_code"]
+    self.assertEqual(response_code, "OK")
+    
+  def test_reach_light_off(self):
     req = requests.get("http://localhost:3000/light_off")
     self.assertEqual(req.status_code, 200)
+    
+  def test_response_light_off(self):
+    req = requests.get("http://localhost:3000/light_off")
+    response_code = req.json()["response_code"]
+    self.assertEqual(response_code, "OK")
+    
+if __name__ == "__main__":
+  unittest.main()
